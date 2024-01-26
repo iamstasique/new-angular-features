f0a1f40d5e75853faf4eb57b3e0dbb153974140b
Stanislau Ihnatsenka <stanislau.ihnatsenka@hiqo-solutions.com>
2024-01-26 12:01:17 +0100

    implement required inputs

--- src/app/pages/protected/protected-child/protected-child.component.ts
+++ src/app/pages/protected/protected-child/protected-child.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-protected-child',
   standalone: true,
   imports: [],
   templateUrl: './protected-child.component.html',
-  styleUrl: './protected-child.component.scss'
+  styleUrl: './protected-child.component.scss',
 })
 export class ProtectedChildComponent {
+  @Input() title: string = 'hello';
+  @Input({ required: true }) requiredTitle: string;
+
   public publicField: string = 'public';
   private privateField: string = 'private';
   protected protectedField: string = 'protected';
